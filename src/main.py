975c21f689cb414f69b4c32c04b7bb6cde30284f
k3y5tr0k3 <bryandonnelly7@gmail.com>
2025-01-05 04:26:57 +0000

    Update main.py with exception handling and logging

--- src/main.py
+++ src/main.py
@@ -2,34 +2,83 @@
 
 import os
 
+from src.utils.logging import Logging
+
 from qiskit import QuantumCircuit
+from qiskit.circuit.exceptions import CircuitError
+from qiskit.providers.exceptions import QiskitBackendNotFoundError
 from qiskit_ibm_runtime import QiskitRuntimeService, SamplerV2 as Sampler
 from qiskit_ibm_runtime.accounts.exceptions import AccountNotFoundError
+from qiskit_ibm_runtime.exceptions import (
+    RuntimeJobFailureError,
+    RuntimeJobMaxTimeoutError,
+    RuntimeInvalidStateError,
+)
 
 
-api_token = os.getenv("IBMQ_API_KEY")
+logger = Logging.get_logger()
+
+API_TOKEN = None
+try:
+    API_TOKEN = os.getenv("IBMQ_API_KEY")
+    assert API_TOKEN is not None
+except AssertionError:
+    logger.error("IBMQ API Token is `None`")
+    exit(100)
 
 
 def setup_test():
     """Checks that the repository is setup and configured correctly."""
     try:
-        service = QiskitRuntimeService(channel="ibm_quantum", token=api_token)
+        service = QiskitRuntimeService(channel="ibm_quantum", token=API_TOKEN)
     except AccountNotFoundError:
-        print(f"API_KEY: {api_token[0:6]}")
+        logger.exception(
+            "Failed to instantiate Qiskit Runtime Service. Check that API"
+            "token is set and is correct."
+        )
+        exit(101)
 
-    backend = service.least_busy(operational=True, simulator=False)
+    else:
+        try:
+            backend = service.least_busy(operational=True, simulator=False)
+        except QiskitBackendNotFoundError:
+            logger.exception("No Qiskit backend found that matches the criteria.")
+            exit(102)
 
-    example_circuit = QuantumCircuit(2)
-    example_circuit.measure_all()
+        else:
+            try:
+                example_circuit = QuantumCircuit(2)
+                example_circuit.measure_all()
+            except CircuitError:
+                logger.exception(
+                    "Failed to instantiate QuantumCircuit. Either the circuit "
+                    "name, if given, is not valid, or both inputs and "
+                    "captures are given."
+                )
+                exit(103)
 
-    sampler = Sampler(backend)
-    job = sampler.run([example_circuit])
+            else:
+                sampler = Sampler(backend)
+                try:
+                    job = sampler.run([example_circuit])
+                except ValueError:
+                    logger.exceptions("Invalid parameter passed to Sampler.run().")
+                    exit(104)
 
-    print(f"job id: {job.job_id()}")
+                else:
+                    logger.info(f"Job id: {job.job_id()}.")
 
-    result = job.result()
+                    result = None
+                    try:
+                        result = job.result()
+                    except RuntimeJobFailureError as e:
+                        logger.exception(f"{e}")
+                    except RuntimeJobMaxTimeoutError as e:
+                        logger.exception(f"{e}")
+                    except RuntimeInvalidStateError as e:
+                        logger.exception(f"{e}")
 
-    return result
+                    return result
 
 
 if __name__ == "__main__":
